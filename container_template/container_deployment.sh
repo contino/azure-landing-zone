756795d1af25e1836e1d40d4271b409ff81919f8
viral.patel <viral.patel@lloydsbanking.com>
2020-04-29 13:35:28 +0100

    finalise the arm templates & created starter for pipeline yaml

--- container_template/container_deployment.sh
+++ container_template/container_deployment.sh
@@ -1,3 +1,3 @@
 az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
 az group create --name demo-acr -l uksouth
-az group deployment create -g demo-acr --template-file ./container_deployment.json --parameters ./container_deployment.parameters.json
+az group deployment create -g demo-acr --template-file ./acrdeploy.json --parameters ./acrdeploy.parameters.json
