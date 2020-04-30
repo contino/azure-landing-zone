70842ad4ce2d2d76a61336ee8579652a625571ef
viral.patel <viral.patel@lloydsbanking.com>
2020-04-30 12:05:56 +0100

    unsupress the error

--- scripts/aksspn.ps1
+++ scripts/aksspn.ps1
@@ -9,7 +9,7 @@ param (
 $spn_exist = Get-AzADApplication -DisplayName $spnName -ErrorAction SilentlyContinue
 
 if($null -eq $spn_exist){
-    $sp = New-AzADServicePrincipal -DisplayName $spnName -ErrorAction SilentlyContinue
+    $sp = New-AzADServicePrincipal -DisplayName $spnName
 
     Write-Host "Adding App ID in Keyvault"
     $appId = $sp.ApplicationId | ConvertTo-SecureString -AsPlainText -Force
