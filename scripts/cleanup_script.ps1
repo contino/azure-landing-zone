8b0d58eee97cbf59632d8b9ac3a83c1c19505f94
Viral Patel <viral.patel@fourth.com>
2020-05-04 17:34:01 +0100

    work on LZ deployment

---
+++ scripts/cleanup_script.ps1
@@ -0,0 +1,4 @@
+az aks delete --name infraaks3vouavntyiekrq --resource-group aks-demo --yes
+az group delete --name acr-demo --yes
+az group delete --name kv-demo --yes
+az group delete --name aks-demo --yes
