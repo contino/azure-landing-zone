92aa340d92ee44907f7aeb2f085a3a17d2c6692c
viralpat218 <viral.patel@fourth.com>
2020-05-06 15:21:51 +0100

    Create Azure Landing Zone Blueprint (#3)
    * work on LZ deployment
    
    * created script to create management group and move sub to specfic management group
    
    * Remove the debug
    
    * creating landing zone keyvault blueprint artifact
    
    * crwte landing zone blueprint
    
    * create pipeline to create mgmt

---
+++ mgmtGroup/mgmtGroup.ps1
@@ -0,0 +1,42 @@
+function New-MgmtGroup
+{
+
+    $file = (Get-ChildItem -Path "./mgmtGroup" | Where-Object {$_.Extension -eq ".json"})
+    $loadVars = Get-Content -Path $file.FullName | ConvertFrom-Json
+    $mgmtGroups = (Get-AzManagementGroup).DisplayName
+
+    foreach($obj in $loadVars.MgmtGroup.GroupName){
+        if ($mgmtGroups -notcontains $obj.name) {
+            if($obj.parent -eq $null) {
+                Write-Host "Creating New Management Group"
+                New-AzManagementGroup -GroupName $obj.name
+
+             if($obj.subscriptionId -ne $null) {
+                 Write-Host "Moving $($obj.subscriptionName) to $($obj.Name)"
+                 New-AzManagementGroupSubscription -GroupName $obj.name -SubscriptionId $obj.subscriptionId
+
+             }
+            }
+            else
+            {
+                $mgmtGroups = (Get-AzManagementGroup).DisplayName
+                if($mgmtGroups -notcontains $obj.parent) {
+                    Write-Host "Creating Parent Management Group"
+                    New-AzManagementGroup -GroupName $obj.name
+                }
+                Write-Host "Creating New Management Group Child"
+                $parentObject = Get-AzManagementGroup -GroupName $obj.parent
+                New-AzManagementGroup -GroupName $obj.Name -ParentObject $parentObject
+
+                if($obj.subscriptionId -ne $null) {
+                    Write-Host "Moving $($obj.subscriptionName) to $($obj.Name) Group"
+                    New-AzManagementGroupSubscription -GroupName $obj.name -SubscriptionId $obj.subscriptionId
+
+                }
+            }
+        }
+
+    }
+}
+
+New-MgmtGroup
---
+++ scripts/blueprint.ps1
@@ -0,0 +1,15 @@
+#Push Blueprint definition to Azure
+
+Import-AzBlueprintWithArtifact -Name "LandingZone" -ManagementGroupId "Management" -InputPath .\landingZone\blueprints -Force
+$bp = Get-AzBlueprint -Name "LandingZone" -ManagementGroupId "Management"
+Publish-AzBlueprint -Blueprint $bp -Version 1.0
+
+# Get the version of the blueprint you want to assign, which we will pas to New-AzBlueprintAssignment
+$publishedBp = Get-AzBlueprint -ManagementGroupId "Management" -Name "LandingZone" -LatestPublished
+
+$parameters = @{
+    "KV-AccessPolicy"="ef044b03-2a5d-4251-96ee-e6065b0d3b3b";
+    "SubscriptionId"="42fd229c-db07-42d3-be34-bc5c98826bc2"
+}
+
+New-AzBlueprintAssignment -Name "LandingZoneAssignment" -Blueprint $publishedBp -Location "UK South" -SubscriptionId "42fd229c-db07-42d3-be34-bc5c98826bc2" -Parameter $parameters
