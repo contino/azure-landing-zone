f553aa741920508a37d076fafc033fe59d90cd75
viralpat218 <viral.patel@fourth.com>
2020-05-07 22:11:47 +0100

    Fix Blueprint script (#6)
    * convert the blueprint.ps1 to parameters and parse the values from azure pipeline
    
    * install blueprint  module
    
    * move the param block to the top
    
    * change assignment name  standard
    
    * change assignment name  standard
    
    * change build number
    
    * remove version param
    
    * remove version param
    
    * fix sub id
    
    * clean up the script
    
    Co-authored-by: viral.patel <viral.patel@lloydsbanking.com>

--- scripts/blueprint.ps1
+++ scripts/blueprint.ps1
@@ -1,21 +1,59 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$BlueprintName,
+    [Parameter(Mandatory=$true)]
+    [String]$mgmtGroupName,
+    [Parameter(Mandatory=$true)]
+    [String]$KVAccessPolicy,
+    [Parameter(Mandatory=$true)]
+    [String]$SubId,
+    [Parameter(Mandatory=$true)]
+    [String]$UserIdentity
+)
+
 Install-Module Az.Blueprint -Force -AllowClobber
 Import-Module Az.Blueprint
 
-#Push Blueprint definition to Azure
+function Set-LZBluePrint {
+    param(
+    [Parameter(Mandatory=$true)]
+    [string]$BlueprintName,
+    [Parameter(Mandatory=$true)]
+    [String]$mgmtGroupName,
+    [Parameter(Mandatory=$true)]
+    [String]$KVAccessPolicy,
+    [Parameter(Mandatory=$true)]
+    [String]$SubId,
+    [Parameter(Mandatory=$true)]
+    [String]$UserIdentity
+    )
 
-Import-AzBlueprintWithArtifact -Name "LandingZone" -ManagementGroupId "Management" -InputPath ./landingZone/blueprints -Force
-$bp = Get-AzBlueprint -Name "LandingZone" -ManagementGroupId "Management"
-Publish-AzBlueprint -Blueprint $bp -Version 1.0
+    $version = Get-Date -format "yyyyMMddssmm"
 
-# Get the version of the blueprint you want to assign, which we will pas to New-AzBlueprintAssignment
-$publishedBp = Get-AzBlueprint -ManagementGroupId "Management" -Name "LandingZone" -LatestPublished
+    $bp_exists = Get-AzBlueprint -Name $BlueprintName -ManagementGroupId $mgmtGroupName -Version $version -ErrorAction SilentlyContinue
+    if($null -eq $bp_exists){
+        Import-AzBlueprintWithArtifact -Name $BlueprintName -ManagementGroupId $mgmtGroupName -InputPath ./landingZone/blueprints -Force
+        $bp = Get-AzBlueprint -Name $BlueprintName -ManagementGroupId $mgmtGroupName
 
-$parameters = @{
-    "KV-AccessPolicy"="ef044b03-2a5d-4251-96ee-e6065b0d3b3b";
-    "SubscriptionId"="42fd229c-db07-42d3-be34-bc5c98826bc2"
+        Write-Host "Publishing Blueprint"
+        Publish-AzBlueprint -Blueprint $bp -Version $version
+
+        # Get the version of the blueprint you want to assign, which we will pas to New-AzBlueprintAssignment
+        $publishedBp = Get-AzBlueprint -ManagementGroupId "Management" -Name "LandingZone" -LatestPublished
+
+        $parameters = @{
+            "KV-AccessPolicy"=$KVAccessPolicy;
+            "SubscriptionId"=$SubId
+        }
+        Write-Host "Assignment Name + $($BlueprintName)$($version)"
+        New-AzBlueprintAssignment -Name "$($BlueprintName)$($version)" -Blueprint $publishedBp -Location "UK South" -SubscriptionId $SubId -Parameter $parameters -UserAssignedIdentity $UserIdentity
+    }
+    else
+    {
+        Write-Host "Blueprint Version exists"
+        Write-Host "Assignment Name + $($BlueprintName)$($version)"
+        Set-AzBlueprintAssignment -Name "$($BlueprintName)$($version)" -Blueprint $bp_exists -ManagementGroupId $mgmtGroupName -Parameter $parameters -UserAssignedIdentity $UserIdentity
+    }
 }
 
-
-$UserAssignedPrincipalId = "/subscriptions/42fd229c-db07-42d3-be34-bc5c98826bc2/resourceGroups/cloud-shell-storage-westeurope/providers/Microsoft.ManagedIdentity/userAssignedIdentities/blueprint"
-
-New-AzBlueprintAssignment -Name "LandingZoneAssignment" -Blueprint $publishedBp -Location "UK South" -SubscriptionId "42fd229c-db07-42d3-be34-bc5c98826bc2" -Parameter $parameters -UserAssignedIdentity $UserAssignedPrincipalId
+Set-LZBluePrint -BlueprintName $BlueprintName -mgmtGroupName $mgmtGroupName -KVAccessPolicy $KVAccessPolicy -SubId $SubId -UserIdentity $UserIdentity
