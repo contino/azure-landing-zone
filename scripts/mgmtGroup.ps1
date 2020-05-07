3b373f4dc6fe9621cf7d287ce4acea9a2d6c596d
viral.patel <viral.patel@lloydsbanking.com>
2020-05-08 00:13:53 +0100

    add try catch around new-azmanagementgroup cmdlet bug with cmdlet

--- scripts/mgmtGroup.ps1
+++ scripts/mgmtGroup.ps1
@@ -4,8 +4,6 @@ function New-MgmtGroup
     $file = (Get-ChildItem -Path "./mgmtGroup" | Where-Object {$_.Extension -eq ".json"})
     $loadVars = Get-Content -Path $file.FullName | ConvertFrom-Json
     $mgmtGroups = (Get-AzManagementGroup).DisplayName
-    Write-Host "List of Management Groups" + $mgmtGroups
-    Write-Host "JSON Object Set ..... +" $loadVars.MgmtGroup.GroupName
 
     foreach($obj in $loadVars.MgmtGroup.GroupName){
         if ($mgmtGroups -notcontains $obj.name) {
@@ -30,11 +28,24 @@ function New-MgmtGroup
                 $mgmtGroups = (Get-AzManagementGroup).DisplayName
                 if($mgmtGroups -notcontains $obj.parent) {
                     Write-Host "Creating Parent Management Group"
-                    New-AzManagementGroup -GroupName $obj.name
+                    try{
+                        New-AzManagementGroup -GroupName $obj.name
+                    }
+                    catch  {
+                        Write-Host $_.Exception.Message
+                    }
                 }
                 Write-Host "Creating New Management Group Child"
                 $parentObject = Get-AzManagementGroup -GroupName $obj.parent
-                New-AzManagementGroup -GroupName $obj.Name -ParentObject $parentObject
+                try
+                {
+                    New-AzManagementGroup -GroupName $obj.Name -ParentObject $parentObject
+                }
+                catch
+                {
+                    Write-Host $_.Exception.Message
+                }
+
 
                 if($obj.subscriptionId -ne $null) {
                     Write-Host "Moving $($obj.subscriptionName) to $($obj.Name) Group"
